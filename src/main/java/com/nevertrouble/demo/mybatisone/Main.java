b5388acaf547d30b0cc04d2a6cc4bc181e3c5bba
lixifun <lixifun@qq.com>
2021-06-29 22:20:27 +0800

    init

---
+++ src/main/java/com/nevertrouble/demo/mybatisone/Main.java
@@ -0,0 +1,10 @@
+package com.nevertrouble.demo.mybatisone;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("======项目启动成功======");
+    }
+}
